7ddd1036d82838a4c1047eca42dfc710804e0868
johnhmacleod <john.h.macleod@gmail.com>
2015-02-24 11:29:05 +1100

    Simplify

--- src/main.c
+++ src/main.c
@@ -7,30 +7,28 @@ int boundsShift = 0;
 void setLayerBounds(TextLayer *subject, int16_t new_bounds)
 {
   APP_LOG(APP_LOG_LEVEL_INFO, "In setter with %d", new_bounds);
-  GRect b;
-  b = layer_get_bounds((Layer *)subject);
-  b.origin.x = new_bounds;
-  layer_set_bounds((Layer *)subject, b);
+  GRect bounds;
+  bounds = layer_get_bounds((Layer *)subject);
+  bounds.origin.x = new_bounds;  // Move the x origin
+  layer_set_bounds((Layer *)subject, bounds);
 };
 
 void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  ++boundsShift;
-  setLayerBounds(s_data_layer, boundsShift);
+  setLayerBounds(s_data_layer, ++boundsShift);
 } 
 
 void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  --boundsShift;
-  setLayerBounds(s_data_layer, boundsShift);
+  setLayerBounds(s_data_layer, --boundsShift);
 }
   
 static void main_window_load(Window *window) {
-  s_data_layer = text_layer_create(GRect(73, 91, 142, 51));
+  s_data_layer = text_layer_create(GRect(73, 91, 144, 51));
   layer_set_frame((Layer *) s_data_layer, GRect(73, 91, 71, 51));
   text_layer_set_background_color(s_data_layer, GColorClear);
   text_layer_set_text_color(s_data_layer, GColorWhite);
-  layer_add_child(window_get_root_layer(s_main_window), text_layer_get_layer(s_data_layer));
+  layer_add_child(window_get_root_layer(s_main_window), (Layer *)s_data_layer);
   text_layer_set_font(s_data_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_MEDIUM_NUMBERS)); 
-  text_layer_set_text(s_data_layer, "890");
+  text_layer_set_text(s_data_layer, "89056");
 }
 
 
@@ -63,4 +61,3 @@ int main(void) {
   app_event_loop();
   deinit();
 }
-
